3f0445164e2a8efa6276d4465225d360a450bf77
Caalb <carlosmunizfjunior@gmail.com>
2024-12-07 21:13:17 -0300

    feat(login): update login form with error handling and validation for username and password

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,15 +14,30 @@
                     <!-- form -->
                     <div class="row justify-content-center">
                         <div class="col-md-10 col-12">
-                            <form action="#" method="post">
+                            <form action="/loginSubmit" method="post" novalidate>
+                                @csrf
                                 <div class="mb-3">
                                     <label for="text_username" class="form-label">Username</label>
-                                    <input type="text" class="form-control bg-dark text-info" name="text_username" required>
+                                    <input type="email" class="form-control bg-dark text-info" name="text_username" value="{{ old('text_username') }}">
                                 </div>
+
+                                @error('text_username')
+                                    <div class="alert alert-danger mt-3">
+                                        <small>{{ $message }}</small>
+                                    </div>
+                                @enderror
+
                                 <div class="mb-3">
                                     <label for="text_password" class="form-label">Password</label>
-                                    <input type="password" class="form-control bg-dark text-info" name="text_password" required>
+                                    <input type="password" class="form-control bg-dark text-info" name="text_password" value="{{ old('text_password') }}">
                                 </div>
+
+                                @error('text_password')
+                                    <div class="alert alert-danger mt-3">
+                                        <small>{{ $message }}</small>
+                                    </div>
+                                @enderror
+
                                 <div class="mb-3">
                                     <button type="submit" class="btn btn-secondary w-100">LOGIN</button>
                                 </div>
@@ -35,6 +50,16 @@
                         <small>&copy; <?= date('Y') ?> Notes</small>
                     </div>
 
+                    {{-- erros --}}
+                    @if($errors->any())
+                        <div class="alert alert-danger mt-3">
+                            <ul class="m-0">
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>

268fb39fbf57043f09f7b60bf92fbbc7089f4c0f
Caalb <carlosmunizfjunior@gmail.com>
2024-12-07 21:13:21 -0300

    feat(routes): add POST route for login submission

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [AuthController::class, 'login']);
 Route::get('/logout', [AuthController::class, 'logout']);
+
+Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);

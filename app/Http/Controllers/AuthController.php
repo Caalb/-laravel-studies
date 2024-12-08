a1a21f93f8cbb0bb7ddb388403588084826365a5
Caalb <carlosmunizfjunior@gmail.com>
2024-12-07 21:13:13 -0300

    feat(AuthController): add loginSubmit method with validation for username and password

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,4 +15,28 @@ class AuthController extends Controller
     {
         echo "";
     }
+
+    public function loginSubmit(Request $request)
+    {
+        // form validation
+        $request->validate(
+            [
+                "text_username" => "required|email",
+                "text_password" => "required|min:6|max:16",
+            ],
+            [
+                "text_username.required" => "O username é obrigatório",
+                "text_username.email" => "O username deve ser um email válido",
+                "text_password.required" => "A password é obrigatória",
+                "text_password.min" => "A password deve ter pelo menos 6 caracteres",
+                "text_password.max" => "A password deve ter no máximo 16 caracteres",
+            ]
+        );
+
+        // ger user input
+        $username = $request->input("text_username");
+        $password = $request->input("text_password");
+
+        echo "OK";
+    }
 }
